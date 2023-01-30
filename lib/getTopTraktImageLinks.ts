8c57332808594c58576ab97868fa39bb069c4681
Manuel Valls Fernandez <me@manvalls.com>
2023-01-31 00:04:52 +0100

    Optimize trakt fetch

--- lib/getTopTraktImageLinks.ts
+++ lib/getTopTraktImageLinks.ts
@@ -4,18 +4,17 @@ import { getShowPoster } from "./getShowPoster"
 
 export const getTopTraktImageLinks = async () => {
   const ratings = await getTraktFavourites()
-  const links = []
   const ids: {[key: string]: boolean} = {}
 
-  for (const rating of ratings) {
+  return Promise.all(ratings.map(async (rating: any) => {
     if (rating.movie && !ids[rating.movie.ids.tmdb]) {
       ids[rating.movie.ids.tmdb] = true
       const imageUrl = await getMoviePoster(rating.movie.ids.tmdb.toString())
       if (imageUrl) {
-        links.push({
+        return {
           imageUrl,
           link: `https://trakt.tv/movies/${rating.movie.ids.slug}`
-        })
+        }
       }
     }
 
@@ -23,13 +22,13 @@ export const getTopTraktImageLinks = async () => {
       ids[rating.show.ids.tmdb] = true
       const imageUrl = await getShowPoster(rating.show.ids.tmdb.toString())
       if (imageUrl) {
-        links.push({
+        return {
           imageUrl,
           link: `https://trakt.tv/shows/${rating.show.ids.slug}`
-        })
+        }
       }
     }
-  }
-
-  return links
+  })).then(
+    (links) => links.filter((link) => link)
+  )
 }
