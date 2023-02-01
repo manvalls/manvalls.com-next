098944a202eb263b852c0b6c3f67ed205b42ab55
Manuel Valls Fernandez <me@manvalls.com>
2023-02-01 18:56:37 +0100

    Revert trakt optimisation to prevent rate limit

--- lib/getTopTraktImageLinks.ts
+++ lib/getTopTraktImageLinks.ts
@@ -4,17 +4,18 @@ import { getShowPoster } from "./getShowPoster"
 
 export const getTopTraktImageLinks = async () => {
   const ratings = await getTraktFavourites()
+  const links = []
   const ids: {[key: string]: boolean} = {}
 
-  return Promise.all(ratings.map(async (rating: any) => {
+  for (const rating of ratings) {
     if (rating.movie && !ids[rating.movie.ids.tmdb]) {
       ids[rating.movie.ids.tmdb] = true
       const imageUrl = await getMoviePoster(rating.movie.ids.tmdb.toString())
       if (imageUrl) {
-        return {
+        links.push({
           imageUrl,
           link: `https://trakt.tv/movies/${rating.movie.ids.slug}`
-        }
+        })
       }
     }
 
@@ -22,13 +23,13 @@ export const getTopTraktImageLinks = async () => {
       ids[rating.show.ids.tmdb] = true
       const imageUrl = await getShowPoster(rating.show.ids.tmdb.toString())
       if (imageUrl) {
-        return {
+        links.push({
           imageUrl,
           link: `https://trakt.tv/shows/${rating.show.ids.slug}`
-        }
+        })
       }
     }
-  })).then(
-    (links) => links.filter((link) => link)
-  )
+  }
+
+  return links
 }
